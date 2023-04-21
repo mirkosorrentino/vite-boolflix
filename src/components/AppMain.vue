ee99f7310aa30fdd33b06bf788b20e9a3112d154
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-21 17:28:41 +0200

    Add: api for images, imported fontawesome

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,9 +11,7 @@ export default{
     },
 
     computed: {
-      isFlagAvailable () {
-        return this.availableFlags.includes (this.cardObj.original_language)
-      }
+      
     },
 
     methods: {
--- src/store.js
+++ src/store.js
@@ -4,5 +4,6 @@ export const store = reactive({
     selectedSearch: "",
     moviesAndTv:[],
     searchKey: "564a7500f244b7fb2c0649f2a8a0940a",
-    apiBase: "https://api.themoviedb.org/3"
+    apiBase: "https://api.themoviedb.org/3",
+    apiImages: "https://image.tmdb.org/t/p/w500/"
 })
