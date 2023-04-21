84eb5063a0e4ef5a0e896b76443809067e87dfca
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-21 16:53:21 +0200

    Add: flags instead of nationality for available flags (it-en)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,8 +5,21 @@ export default{
     name: "AppMain",
     data(){
         return{
-          store
+          store,
+          availableFlags: ['it', 'en']
         }
+    },
+
+    computed: {
+      isFlagAvailable () {
+        return this.availableFlags.includes (this.cardObj.original_language)
+      }
+    },
+
+    methods: {
+      getImgPath (imgPath) {
+        return new URL(imgPath, import.meta.url).href;
+      }
     }
 }
 </script>
@@ -15,10 +28,15 @@ export default{
   <main>
     <ul>
       <li v-for="movie in store.movies">
-        <h3> {{ movie.title }}</h3>
-        <div> {{ movie.original_title }} </div>
-        <div> {{ movie.original_language }} </div>
-        <div> {{ movie.vote_average }} </div>
+        <div><span class="ms_category">Titolo</span>: {{ movie.title }}</div>
+        <div><span class="ms_category">Titolo originale</span>: {{ movie.original_title }} </div>
+        <div class="ms_nationality">
+          <span class="ms_category">Lingua originale</span>
+          <span v-if="availableFlags.includes(movie.original_language)">: <img :src="getImgPath(`../assets/img/${movie.original_language}.png`)" class="ms_flag" alt="flag"></span>
+          <span v-else>: {{ movie.original_language }}</span>
+        </div>
+        <div><span class="ms_category">Voto</span>: {{ movie.vote_average }} </div>
+        <div><span class="ms_category">Overview</span>: {{ movie.overview }}</div>
       </li>
     </ul>
   </main>
@@ -27,5 +45,13 @@ export default{
 <style scoped lang="scss">
 @use "../styles/partials/mixins.scss";
 @use "../styles/partials/variables.scss";
+
+.ms_category {
+  font-weight: 700;
+}
+
+.ms_flag {
+  width: 5%;
+}
 
 </style>
